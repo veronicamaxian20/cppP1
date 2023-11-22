3e3a60bbfd1c723fea28664f49c44b71f59440e5
Veronica Maxian <veronicamaxian@gmail.com>
2023-11-22 23:30:59 +0200

    added tickets class

---
+++ MyProject/Tickets.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Ticket.h"
+
+class Tickets {
+private:
+	int nrTickets;
+	Ticket** tickets;
+
+public:
+	int getNrTickets() const; //const p/u ca nu vrem sa se modif nimic
+	Tickets** getTickets() const;
+
+	void setTickets(int, Ticket*);
+
+	Tickets();
+	Tickets(int, Ticket*);
+	Tickets(const Tickets&);
+	~Tickets();
+	Tickets& operator=(const Tickets&);
+
+	//validation for tickets
+	//add ticket
+	//display ticket
+};
